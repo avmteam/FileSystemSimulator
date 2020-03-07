f8ee6f0a1a8e022a8f81a28a4399e6e26000bbcc
scientist19 <40214489+scientist19@users.noreply.github.com>
2020-03-07 19:53:35 +0200

    created solution and projects

---
+++ FileSystemSimulator/FileSystemSimulator/FileSystemSimulator.cpp
@@ -0,0 +1,10 @@
+﻿// FileSystemSimulator.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
---
+++ FileSystemSimulator/Shell/pch.h
@@ -0,0 +1,6 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+// TODO: add headers that you want to pre-compile here
+
+#endif //PCH_H
