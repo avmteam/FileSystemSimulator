baf8906b8d399214a5d84aa432c98a73224de002
MaiiaChudinova <maya.chudinova@unicyb.kiev.ua>
2020-05-17 20:10:14 +0300

    Added FileInfo structure, declared directory method

--- FileSystemSimulator/FileSystem/FileSystem.h
+++ FileSystemSimulator/FileSystem/FileSystem.h
@@ -9,6 +9,7 @@ class FileSystem
 {
 public:
 
+
   struct DirEntry {
     static const size_t MAX_FILE_NAME_LENGTH = 16;
 
@@ -19,6 +20,17 @@ public:
 
     char file_name[MAX_FILE_NAME_LENGTH];
     size_t file_descr_index;
+  };
+
+  struct FileInfo {
+
+	  FileInfo(const std::string& i_file_name, size_t i_file_length) {
+		  std::memcpy(file_name, i_file_name.c_str(), MAX_FILE_NAME_LENGTH);
+		  file_length = i_file_length;
+	  }
+
+	  char file_name[DirEntry::MAX_FILE_NAME_LENGTH];
+	  size_t file_length;
   };
 
 public:
@@ -32,6 +44,7 @@ public:
   bool read(size_t index, char* mem_area, size_t count);
   bool write(size_t index, char* mem_area, size_t count);
   bool lseek(size_t index, size_t pos);
+  FileInfo[] directory();
 
 private:
   int findFreeFileDescriptor();
