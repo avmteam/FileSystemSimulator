65fc7b674f66fb1380ac72f791b1aa96ac0a81ce
alexandrakmet <alexandrakmett@gmail.com>
2020-05-25 04:33:06 +0300

    last design commit

--- FileSystemSimulator/Shell/main.cpp
+++ FileSystemSimulator/Shell/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include "Shell.h"
+#include <Windows.h>
+
 
 using namespace std;
 
@@ -12,11 +14,13 @@ int main()
 	shell.printHelp();
 
 	string command;
-
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	do
 	{
+		SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 15));
 		cout << "> ";
 		getline(cin, command);
+		SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 14));
 
 	} while (shell.parseCommand(command) != shell.exit_code);
 }
